b7efa6d9f7abf3beece46c43a0b7ff88a52bf70c
ABHINAtk <abhinashaji@gmail.com>
2023-11-28 19:30:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TemplateDrivenForms';
+
+  form = {
+    username : '',
+    email : '',
+    address : '',
+    dob : '',
+    password : '',
+    phone : ''
+
+  }
+
+  alphabetregex = /^[a-zA-Z]+$/
+  regex= /^[a-zA-Z]+$/ 
+  integerRegex =/^\d+$/
+  passwordreg =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+  passreg = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+  Dateregex=/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
+
+  
+
+
+  registerFn(){
+    console.log(this.form)
+  }
 }
